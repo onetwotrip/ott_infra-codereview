94cf6ebf38f11c77ebcf5ee2959104be3b91b08b
Dmitry Shmelev <avikez@gmail.com>
2016-01-12 17:07:50 +0300

    Add start point of codereview module

--- lib/ott_infra/codereview.rb
+++ lib/ott_infra/codereview.rb
@@ -2,6 +2,9 @@ require "ott_infra/codereview/version"
 
 module OttInfra
   module CodeReview
+    def self.run
+      true
+    end
     # Your code goes here...
   end
 end
--- spec/lib/codereview_spec.rb
+++ spec/lib/codereview_spec.rb
@@ -2,5 +2,7 @@ require "spec_helper"
 require "ott_infra/codereview"
 
 describe OttInfra::CodeReview do
-
+  it "start point is exist" do
+    expect( OttInfra::CodeReview.run ).to be true
+  end
 end
