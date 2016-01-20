ed238cd7d940de6187b50251b6aa67dd3b976837
Dmitry Shmelev <avikez@gmail.com>
2016-01-20 18:29:33 +0300

    Add codeclimate coverage tool

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,9 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'simplecov'
 SimpleCov.start do
     add_filter "/spec/"
